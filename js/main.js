98e16aa94db7e93a4055a793002be8d7ba6c2282
calgil <clgilbert1105@gmail.com>
2022-04-27 22:01:48 -0600

    created gallery section

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const switcherBtn = '.switcher-btn';
 const dark = 'dark';
 const light = 'light';
 const open = 'open';
-const active = '.active';
+const active = 'active';
 
 
 const root = document.documentElement;
@@ -16,6 +16,36 @@ const toggleTheme = document.querySelector(themeTab);
 const switcher = document.querySelectorAll(switcherBtn);
 const currentTheme = localStorage.getItem(theme);
 
+const setActive = (elm, selector) => {
+    if(document.querySelector(`${selector}.${active}`) !== null) {
+        document.querySelector(`${selector}.${active}`).classList.remove(active);
+    } 
+    elm.classList.add(active);
+}
+
+const setTheme = (val) => {
+    if(val === dark) {
+        root.setAttribute(dataTheme, dark);
+        localStorage.setItem(theme, dark);
+    } else {
+        root.setAttribute(dataTheme, light);
+        localStorage.setItem(theme, light);
+    }
+};
+
+if (currentTheme) {
+    root.setAttribute(dataTheme, currentTheme);
+    switcher.forEach((btn) => {
+        btn.classList.remove(active);
+    });
+
+    if(currentTheme === 'dark'){
+        switcher[1].classList.add(active);
+    } else {
+        switcher[0].classList.add(active);
+    }
+};
+
 toggleTheme.addEventListener('click', function() {
     const tab = this.parentElement.parentElement;
     if(!tab.className.includes(open)) {
@@ -24,3 +54,11 @@ toggleTheme.addEventListener('click', function() {
         tab.classList.remove(open);
     }
 });
+
+for (const elm of switcher){
+    elm.addEventListener('click', function() {
+        const toggle = this.dataset.toggle;
+        setActive(elm, switcherBtn);
+        setTheme(toggle);
+    })
+};
